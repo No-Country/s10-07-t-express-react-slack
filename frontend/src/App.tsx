2bf4401bb0b73f65f259f50026304af5be696646
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-17 20:56:04 -0300

    client connection socket.io

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import io from 'socket.io-client'
 
+const socket = io("/");
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
+        HOLA MUNDO
       </h1>
     </>
   )
