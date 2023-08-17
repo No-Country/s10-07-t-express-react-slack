ebad06523a7880a1c58ab5f899be7a7e0aa635eb
Cemb93 <karloz13m@gmail.com>
2023-08-17 15:28:34 -0500

    mensaje inicial socket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,24 @@ import { app } from "./app";
 import { dbConexion } from "./db";
 const { PORT } = process.env;
 console.log("PORT:", PORT)
+import { createServer } from "http";
+import { Server as SocketServer } from "socket.io"
+
+const server = createServer(app);
+
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+io.on("Connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("message", (data: any) => {
+    console.log("DATA:", data)
+  })
+})
 
 dbConexion().then(() => {
   app.listen(PORT, () => {
