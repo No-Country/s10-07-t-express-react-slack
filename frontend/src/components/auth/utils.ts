3903736549340c945adf0bf3bf9dd534b3848776
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-21 16:37:02 -0300

    add validations

--- frontend/src/components/auth/utils.ts
+++ frontend/src/components/auth/utils.ts
@@ -2,10 +2,22 @@ import { Errors, Register } from './interfaces';
 
 export const validationErrors = (formLogin:Register) => {
   let errors: Errors = {};
-  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const fullName = /^[a-zA-ZáéíóúÁÉÍÓÚüÜ]{3,}(?:\s|-)[a-zA-ZáéíóúÁÉÍÓÚüÜ]{3,}$/;
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const dominiosPermitidos = ['gmail.com', 'hotmail.com', "yahoo.com", "yahoo.es", "outlook.com", "outlook.es"];
+  const dominiosPermitidosRegex = new RegExp(`^[a-zA-Z0-9._%+-]+@(${dominiosPermitidos.join('|')})$`, 'i');
+
   const regexPasswordMayuscula = /(?=.*[A-Z])/;
   const regexPasswordEspecialCharacter = /(?=.*[!@#$%^&*])/;
 
+  if (!formLogin.fullName.length) {
+    errors.fullName = "Por favor ingresa tu nombre completo.";
+  }
+
+  if (!fullName.test(formLogin.fullName)) {
+    errors.fullName = "Debe contener al menos 2 nombres y un maximo de 4. Cada uno de ellos separados por un espacio en blanco.";
+  }
+  
   if (!formLogin.email.length) {
     errors.email = "Por favor ingresa tu email.";
   }
@@ -14,8 +26,16 @@ export const validationErrors = (formLogin:Register) => {
     errors.email = "Por favor ingresa un email válido.";
   }
 
+  if (!dominiosPermitidosRegex.test(formLogin.email)) {
+    errors.email = "Por favor ingresa un dominio de email válido.";
+  }
+
   if (!formLogin.password.length) {
     errors.password = "Por favor ingresa una contraseña.";
+  }
+
+  if (formLogin.password.length < 8) {
+    errors.password = "La contraseña debe tener al menos 8 caracteres.";
   }
 
   if (!regexPasswordMayuscula.test(formLogin.password)) {
