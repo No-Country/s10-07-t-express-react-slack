ae24b5e8da7dd4bf084c9bd06dca4781a57e41b9
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-24 15:26:23 -0300

    create axios interfaces

--- frontend/src/components/auth/interfaces.ts
+++ frontend/src/components/auth/interfaces.ts
@@ -1,13 +1,37 @@
+import { AxiosError, AxiosResponse } from 'axios';
+
 export interface Register {
-    fullName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface Login {
+  email: string;
+  password: string;
 }
 
 export interface Errors {
-    fullName?: string;
-    email?: string;
-    password?: string;
-    confirmPassword?: string;
+  fullName?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+interface DataResponse {
+  error?: string;
+  existUser?: Register;
+  token?: string;
+}
+
+interface MyApiResponse {
+  data: DataResponse;
+  status: number;
+  statusText: string;
+  headers: any;
+}
+
+export interface MyAxiosError extends AxiosError {
+  response?: AxiosResponse<MyApiResponse>; 
 }
