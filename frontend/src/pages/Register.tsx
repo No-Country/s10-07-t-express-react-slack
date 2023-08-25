bfab053e46cc6908a761e3df052b47f279a4545f
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-25 16:02:46 -0300

    add navbar at register route

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,13 +1,15 @@
+import NavBar from "../components/NavBar";
 import Form from "../components/auth/Form";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const Register = () => {
   return(
-    <>
+    <section className="bg-bg">
+      <NavBar isUserLoggedIn={false}/>
       <GoogleOAuthProvider clientId="1003915463020-v12qk1fvdg1smd9skp2i9vrv5j99tra6.apps.googleusercontent.com">
-        <Form/>  
+        <Form/> 
       </GoogleOAuthProvider>
-    </>
+    </section>
   )
 }
 export default Register;
