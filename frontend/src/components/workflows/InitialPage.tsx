dfedf5bb4c299ced2c2603254774e7d283aaacf1
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-29 11:55:56 -0300

    complete flow rtk

--- frontend/src/components/workflows/InitialPage.tsx
+++ frontend/src/components/workflows/InitialPage.tsx
@@ -1,8 +1,16 @@
 // import axios from "axios";
 import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { increment } from "../../redux/slices/user.slice";
 // import { ResponseAxios } from "../auth/interfaces";
 
 const InitialPage = () => {
+
+  const dispatch = useAppDispatch()
+  const count = useAppSelector((state) => state.user.value)
+  const handle = () => {
+    dispatch(increment())
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("userToken") as string;
@@ -17,7 +25,8 @@ const InitialPage = () => {
   return (
     <section>
       <h2 className=""> Estos son sus espacios de trabajo</h2>
-      <button className="border px-4 py-2 rounded-md">Crear espacio de trabajo</button>
+      <button className="border px-4 py-2 rounded-md" onClick={handle}>+</button>
+      <span>{count}</span>
     </section>
   )
 }
