656044b0dc71e2867acbe282a858d88b92ddb996
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-30 14:14:20 -0300

    add global methods and states

--- frontend/src/components/workflows/InitialPage.tsx
+++ frontend/src/components/workflows/InitialPage.tsx
@@ -1,34 +1,24 @@
-// import axios from "axios";
 import { useEffect } from "react";
-// import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-// import { increment } from "../../redux/slices/user.slice";
-// import { ResponseAxios } from "../auth/interfaces";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import chicoWorkspace from "../../assets/chico-workspace.png"
 import chicaWorkspace from "../../assets/chica-workspace.png"
 import { Link } from "react-router-dom";
+import { validateUser } from "../../redux/slices/user.slice";
 
 const InitialPage = () => {
 
-  // const dispatch = useAppDispatch()
-  // const count = useAppSelector((state) => state.user.value)
-  // const handle = () => {
-  //   dispatch(increment())
-  // }
+  const dispatch = useAppDispatch()
+  const {email} = useAppSelector((state) => state.user)
 
   useEffect(() => {
-    const token = localStorage.getItem("userToken") as string;
-    // const response = axios("http://localhost:3001/auth", {
-    //   headers: {
-    //     'Authorization': `Bearer ${token}`
-    //   }
-    // })
-    console.log(token)
+    dispatch(validateUser())
   }, [])
 
   return (
     <section className="w-full py-24 flex items-center justify-center bg-background">
       <div className="relative w-[55em] flex items-center justify-center px-20 py-12 bg-background-container-workspace text-text-workspace rounded-2xl">
         <div className="flex flex-col gap-y-6 w-96">
+      <h3>{email}</h3>
           <h3 className="text-2xl font-semibold">Crea un nuevo espacio de trabajo</h3>
           <p>
             Crea un nuevo espacio de trabajo, un lugar donde poder conversar
