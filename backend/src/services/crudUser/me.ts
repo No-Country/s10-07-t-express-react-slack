51f9b7d37ada1207c5505ed1bfcae2ec34fe6ebe
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-08-30 14:12:00 -0300

    change return data

--- backend/src/services/crudUser/me.ts
+++ backend/src/services/crudUser/me.ts
@@ -4,22 +4,20 @@ import { IPayload } from "../../../../interface/IPayload";
 import { Usermodel } from "../../models/Users";
 const { TOKEN } = process.env
 
-export const me = async(req: Request, res: Response) => {
-    if (req.headers['authorization']) {
-        try {
-          const token = req.headers['authorization']
-          const payload = Jwt.verify(token, TOKEN as string) as IPayload
-          const userData = await Usermodel.findOne({
-           email: payload.id
-          }).select('-password')
-          const data = {
-            userData
-          }
-          return res.status(200).json({data})
-    
-        } catch (error) {
-          return res.status(400).json({ msg: "Token inválido" })
-        }
-      }
-      return res.status(404).json({msg: "Token no enviado"})
+export const me = async (req: Request, res: Response) => {
+  if (req.headers['authorization']) {
+    try {
+      const token = req.headers['authorization']
+      console.log(token);
+      const payload = Jwt.verify(token, TOKEN as string) as IPayload
+      const userData = await Usermodel.findOne({
+        email: payload.id
+      }).select('-password')
+
+      return res.status(200).json({ data: userData })
+    } catch (error) {
+      return res.status(400).json({ msg: "Token inválido" })
+    }
+  }
+  return res.status(404).json({ msg: "Token no enviado" })
 }
