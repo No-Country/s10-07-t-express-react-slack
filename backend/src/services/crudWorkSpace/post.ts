c73c625b272e235f7463c69fa56fd6264ea7cb98
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-09-06 12:22:25 -0300

    create a default channel

--- backend/src/services/crudWorkSpace/post.ts
+++ backend/src/services/crudWorkSpace/post.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { IWorkSpace } from "../../../../interface/IWorkSpace";
 import { WorkSpaceModel } from "../../models/WorkSpace";
 import { validateWorkSpace } from "../../validations/workSpace";
+import { ChannelsModel } from "../../models/Channels";
 
 export const workSpace = async (req: Request, res: Response) => {
   const workSpace = req.body as IWorkSpace;
@@ -11,17 +12,26 @@ export const workSpace = async (req: Request, res: Response) => {
 
 
     const existWorkSpace = await WorkSpaceModel.findOne({ nameWorkSpace: validations.nameWorkSpace });
-
+    
     if (existWorkSpace) {
       return res.status(401).json({ error: "Este espacio de trabajo ya existe" })
     }
 
-    const newWorkSpace = new WorkSpaceModel({
+    const newWorkSpace = await WorkSpaceModel.create({
       userId: workSpace.userId,
       nameWorkSpace: validations.nameWorkSpace,
       // fullName: workSpace.fullName
     });
-    await newWorkSpace.save();
+    
+    const defaultChannel = await ChannelsModel.create({
+      workSpaceId: newWorkSpace._id
+    });
+
+    await defaultChannel.save();
+
+    const channelPush = []
+    channelPush.push(defaultChannel._id)
+    await WorkSpaceModel.updateOne({_id: newWorkSpace._id}, {channelsId: channelPush})
 
     if (newWorkSpace) {
       return res.status(201).json({
