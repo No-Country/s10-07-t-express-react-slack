783a1d6ac9b1000930c1a18f348a1d44ddd32980
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-09-06 12:20:56 -0300

    create endpoint get a one workspace

---
+++ backend/src/services/crudWorkSpace/getONeWorkspace.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import { WorkSpaceModel } from "../../models/WorkSpace"
+
+export const getOneWorkspace = async (req: Request, res: Response) => {
+    const id = req.params.id
+  try {
+    const workSpace = await WorkSpaceModel.findById({_id: id})
+    if (workSpace) {
+      return res.json({data: workSpace})
+    }
+    return res.json({ msg: "No existe el espacio de trabajo" })
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message })
+    }
+  }
+}
