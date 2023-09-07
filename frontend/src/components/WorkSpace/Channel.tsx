e242ead7cbf8f61a43b51064837674b8580e047c
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-09-07 10:31:10 -0300

    edit channel

--- frontend/src/components/WorkSpace/Channel.tsx
+++ frontend/src/components/WorkSpace/Channel.tsx
@@ -5,7 +5,9 @@ import { generateId } from './utils';
 import { validateUser } from '../../redux/slices/user.slice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import axios from 'axios';
-// import axios from 'axios';
+
+import rightArrow from '../../assets/rightArrow.svg'
+import {AiOutlinePlus} from 'react-icons/ai'
 
 // Ejemplo de como implementar socket 
 // https://github.com/CodenautaJorge/React-socketio-chat/blob/main/client/src/App.js
@@ -65,21 +67,52 @@ const Channel = () => {
   };
 
   return (
-    <section>
-      <div>
+    <section className='mt-20 py-8 px-12 w-2/3 h-screen flex flex-col items-center justify-between'>
+      <div className='w-full flex flex-col gap-y-4'>
+        <div className='w-full flex items-center justify-between'>
+          <div className='flex items-center font-semibold gap-x-3 text-2xl'>
+            <span className='text-button-orange text-3xl'>Canales</span>
+            <img src={rightArrow} className='w-3'/>
+            <span className='text-[#828282]'>#Nombre Canal</span>
+          </div>
+          <button className='text-black flex items-center gap-x-2'>
+            <div className=''>
+              <AiOutlinePlus/>
+            </div>
+            <span>Añadir descripcion</span>
+          </button>
+        </div>
+        <div className='w-full flex flex-col gap-y-4'>
+          <div className='h-[1px] bg-[#656464]/40 w-full'></div>
+          <button className='text-black flex items-center gap-x-2'>
+            <div className=''>
+              <AiOutlinePlus/>
+            </div>
+            <span>Añadir enlaces</span>
+          </button>
+          <div className='h-[1px] bg-[#656464]/40 w-full'></div>
+        </div>
+      </div>
+      <div className='w-full'>
         {messages.map((data) => (
-          <div>
-            <span className='bg-red-600 px-2 py-1 rounded-lg'>{data.userId}</span>
-            <div key={generateId()}>{data.message}</div>
+          <div key={generateId()} className='flex gap-x-4'>
+            <img src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" className='w-16 h-16' alt="" />
+            <div className='flex flex-col gap-y-4'>
+              <span className='font-semibold text-xl text-[#555454]/80'>Nombre usuario</span>
+              <div>{data.message}</div>
+            </div>
           </div>
         ))}
       </div>
-      <input
-        type="text"
-        value={inputMessage}
-        onChange={(e) => setInputMessage(e.target.value)}
-      />
-      <button onClick={handleSendMessage}>Send</button>
+      <div>
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          className='border border-black'
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </section>
   )
 }
