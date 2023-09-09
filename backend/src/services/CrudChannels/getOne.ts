51f4ca3d401b3870b84a77a590c3554f2cc4591c
Sonny <spimentelm1201@gmail.com>
2023-09-09 14:34:10 -0500

    getOneChannel with messages added

--- backend/src/services/CrudChannels/getOne.ts
+++ backend/src/services/CrudChannels/getOne.ts
@@ -5,9 +5,9 @@ export const getOneChannel = async (req: Request, res: Response) => {
   try {
     const channel = await ChannelsModel.findById(req.params.idChannel).populate({path:'messages', options:{ sort: { 'createdAt': -1 } }})
     if (channel) {
-      return res.json(channel)
+      return res.status(200).json(channel)
     }
-    return res.json({ msg: "El canal no existe" })
+    return res.status(404).json({ msg: "El canal no existe" })
   } catch (error) {
     if (error instanceof Error) {
       return res.status(400).json({ error: error.message })
