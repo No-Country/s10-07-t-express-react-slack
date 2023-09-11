d966fa23ac55568f911febaa38b14ba9c1b05eb4
adrianffigueroa <adrianffigueroa@gmail.com>
2023-09-11 14:20:09 -0300

    trabajando en el back y sockets de mensajes

--- backend/src/services/crudMessage/post.ts
+++ backend/src/services/crudMessage/post.ts
@@ -1,48 +1,42 @@
-import { Request, Response } from "express";
-import { IMessage } from "../../../../interface/IMessage";
-import { MessageModel } from "../../models/Message";
-import { ChannelsModel } from "../../models/Channels";
-import { io } from "../../index";
+import { Request, Response } from 'express'
+import { IMessage } from '../../../../interface/IMessage'
+import { MessageModel } from '../../models/Message'
+import { ChannelsModel } from '../../models/Channels'
+import { io } from '../../index'
 
 export const createMessage = async (req: Request, res: Response) => {
-
-
   // const { from, message } = req.body;
-  const {message, workSpaceId, channelsId, userId} = req.body as IMessage;
+  const { message, workSpaceId, channelsId, userId } = req.body as IMessage
 
   try {
-
-
     const data = new MessageModel({
       workSpaceId,
       message,
       userId,
-      channelsId
+      channelsId,
       // from: message.from,
-
-    });
-    await data.save();
+    })
+    await data.save()
     // res.status(201).json(newMessage);
-    
-    io.on("connection", (socket) => {
-      console.log("Connected with socket");
-      socket.on(`${workSpaceId}/${channelsId}`, (data: any) => {
-        socket.broadcast.emit(`${workSpaceId}/${channelsId}`, data)
-      })
-    });
+
+    // io.on("connection", (socket) => {
+    //   console.log("Connected with socket");
+    //   socket.on(`${workSpaceId}/${channelsId}`, (data: any) => {
+    //     socket.broadcast.emit(`${workSpaceId}/${channelsId}`, data)
+    //   })
+    // });
 
     if (data) {
       return res.status(201).json({
-        message: "Se Creo con exito el mensaje",
-        data: data
+        message: 'Se Creo con exito el mensaje',
+        data: data,
       })
     }
-    
+
     // } catch (error) {
     //   res.status(500).json({ error: 'Error al guardar el mensaje' });
     // }
     // }
-
   } catch (error) {
     if (error instanceof Error)
       return res.status(400).json({ error: error.message })
