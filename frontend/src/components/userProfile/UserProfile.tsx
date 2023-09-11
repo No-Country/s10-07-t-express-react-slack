5c7c0b56a1680a76fcf2e557625dbc77d4afbb5a
AlondraCena <alondra.cena@gmail.com>
2023-09-11 06:48:14 -0300

    put route name input

--- frontend/src/components/userProfile/UserProfile.tsx
+++ frontend/src/components/userProfile/UserProfile.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { FaTimes, FaCamera, FaRegEdit } from "react-icons/fa";
 import { useSelector } from 'react-redux';
+import Axios from "axios";
 
 const UserProfile = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
+  const [errorUpdateMessage, setErrorUpdateMessage] = useState("")
+  const [successUpdateMessage, setSuccessUpdateMessage] = useState("")
 
+  const id = useSelector((state) =>  state.user._id);
   const name = useSelector((state) =>  state.user.fullName);
   const email = useSelector((state) =>  state.user.email);
   const profileImage = useSelector((state) =>  state.user.profileImage);
+  console.log(name, id);
+  
+  const handleUpdateUser = async () => {
+    const userData = {fullName: editedName} 
+    try {
+      const response = await Axios.put(`http://localhost:3001/user/${id}`, userData
+        //fullName: editedName, // Reemplaza "nuevoNombre" con el nuevo nombre del usuario.
+        //profileImage: nuevaImagen, // Reemplaza "nuevaImagen" con la nueva imagen del usuario.
+      );
+      setSuccessUpdateMessage(response.data.msg)
+      setIsEditingName(false);
+    } catch (error) {
+      if(error){setErrorUpdateMessage("Error al actualizar el usuario")}
+    }
+  };
+
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [editedName, setEditedName] = useState(name);
+  console.log(editedName);
+  
+  const handleEditName = () => {
+    setIsEditingName(true);
+  };
+
 
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-black opacity-40"></div>
-      <div className="z-50 bg-bg-profile rounded-lg p-6 h-96 w-80 items-center">
-        {/* Contenido del modal */}
+      <div className="z-50 bg-bg-profile rounded-lg p-6 h-auto w-80 items-center">
         <div className="flex justify-between">
         <h2 className="font-semibold text-2xl text-button-orange">Mi perfil</h2>
-        {/* Agrega aquí el contenido de tu modal */}
         <button className=" text-white rounded " onClick={onClose}>
           <FaTimes className="hover:text-button-orange"/>
         </button>
@@ -43,10 +69,21 @@ const UserProfile = ({ isOpen, onClose }) => {
         </div>
         <div className='bg-white h-[1px] w-full mt-8'></div>
         <div className="mt-2"> 
-            <h2 className="text-white text-xl font-medium mt-2 flex">{name}  <FaRegEdit className="ml-2 mt-1"/></h2>
+        
+      {isEditingName ? (
+        <div className="flex items-center">
+          <input type="text" className="bg-white border text-black placeholder:text-background text-sm rounded-xl block w-full pl-10 p-1.5" value={editedName} onChange={(e) => setEditedName(e.target.value)}/>
+          <button onClick={() => setIsEditingName(false)}><FaTimes className="text-white ml-2 hover:text-button-orange"/></button>
+        </div>
+      ) : (
+        <h2 className="text-white text-xl font-medium mt-2 flex">{name} <FaRegEdit className="ml-2 mt-1" onClick={handleEditName} /></h2>
+        )}
             <h2 className="text-white text-lg mt-3">Correo electrónico:</h2>
             <p className="text-white text-base mt-">{email}</p>
         </div>
+      <button className="bg-button-orange rounded-xl text-white font-semibold px-4 w-full py-1.5 mt-2" onClick={handleUpdateUser}>Guardar cambios</button>
+      {errorUpdateMessage && <p className='text-red-600 font-semibold mt-4'>Error: {errorUpdateMessage}</p>}
+      {successUpdateMessage && <p className='text-green-600 font-semibold mt-4'>{successUpdateMessage}</p>}
       </div>
     </div>,
     document.getElementById("modal-root")
