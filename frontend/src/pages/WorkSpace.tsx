3c0d07263e653de7bbec9b31c17cbadf0335a453
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-09-11 10:15:06 -0300

    validate user

--- frontend/src/pages/WorkSpace.tsx
+++ frontend/src/pages/WorkSpace.tsx
@@ -6,12 +6,14 @@ import Footer from "../components/footer/Footer";
 import { useAppDispatch } from '../redux/hooks';
 import { getOneWorkspace } from '../redux/slices/workspace.slice';
 import { useParams } from 'react-router-dom';
+import { validateUser } from '../redux/slices/user.slice';
 
 const WorkSpace = () => {
   const {id} = useParams<string>()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
+    dispatch(validateUser())
     dispatch(getOneWorkspace(id))
   }, [])
 
